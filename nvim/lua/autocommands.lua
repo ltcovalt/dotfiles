cf3541e0a8999dff7466b0fd48dfafd387e048e9
Luke Covalt <ltcovalt@gmail.com>
2025-11-20 20:03:32 -0500

    configure spellcheck & prettier

--- nvim/lua/autocommands.lua
+++ nvim/lua/autocommands.lua
@@ -1,9 +1,18 @@
 -- Highlight when yanking (copying) text
 --  See `:help vim.hl.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.hl.on_yank()
-  end,
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.hl.on_yank()
+	end,
+})
+
+-- Enaable built-in spell checker for markdown
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "markdown", "markdown.mdx" },
+	callback = function()
+		vim.opt_local.spell = true
+		vim.opt_local.spelllang = { "en_us" }
+	end,
 })
