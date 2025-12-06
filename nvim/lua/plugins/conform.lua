d15014b8a00bd32def00aef0503d37241d3a8722
Luke Covalt <ltcovalt@gmail.com>
2025-12-05 21:32:13 -0500

    nvim config update

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -34,6 +34,7 @@ return { -- Autoformat
 			--
 			-- You can use 'stop_after_first' to run the first available formatter from the list
 			-- javascript = { "prettierd", "prettier", stop_after_first = true },
+			css = { "prettierd", "prettier", stop_after_first = true },
 			javascript = { "prettierd", "prettier", stop_after_first = true },
 			json = { "prettierd", "prettier", stop_after_first = true },
 			lua = { "stylua" },
