797c0a86deab33bf9d53801156b3cc09e448e720
Luke Covalt <ltcovalt@gmail.com>
2026-01-25 22:22:34 -0500

    update treesitter config

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,32 +1,44 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	lazy = false,
 	build = ":TSUpdate",
 	config = function()
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = {
-				"bash",
-				"c",
-				"diff",
-				"html",
-				"lua",
-				"luadoc",
-				"markdown",
-				"markdown_inline",
-				"query",
-				"vim",
-				"vimdoc",
-				"astro",
-				"typescript",
-				"css",
-				"javascript",
-				"tsx",
-			},
-			auto_install = true,
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = { "ruby" },
-			},
-			indent = { enable = true, disable = { "ruby" } },
+		local ts = require("nvim-treesitter")
+		local install_languages = {
+			"bash",
+			"c",
+			"diff",
+			"html",
+			"lua",
+			"luadoc",
+			"markdown",
+			"markdown_inline",
+			"query",
+			"vim",
+			"vimdoc",
+			"astro",
+			"typescript",
+			"css",
+			"javascript",
+			"tsx",
+		}
+
+		ts.setup({
+			install_dir = vim.fn.stdpath("data") .. "/site",
+		})
+
+		ts.install(install_languages)
+
+		vim.api.nvim_create_autocmd("FileType", {
+			callback = function(args)
+				local ft = vim.bo[args.buf].filetype
+				if vim.tbl_contains(install_languages, ft) then
+					vim.treesitter.start(args.buf)
+					if ft ~= "ruby" then
+						vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+					end
+				end
+			end,
 		})
 	end,
 }
